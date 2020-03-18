03e3590fca4070a70899a6a381b58d571a0b8c9f
pvanbranteghen <pvanbranteghen@stratio.com>
2020-03-18 17:33:05 +0100

    removed solution

--- src/python/project/bin/run.py
+++ src/python/project/bin/run.py
@@ -1,5 +1,5 @@
 from data.make_dataset  import download_raw_data
-from models.train_model import mlflow_model
+#from models.train_model import mlflow_model
 import sys
 
 
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
     data = download_raw_data()
     
-    mlflow_model(data,alpha,l1_ratio)
+    #mlflow_model(data,alpha,l1_ratio)
     
